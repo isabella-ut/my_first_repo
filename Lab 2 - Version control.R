afba56a209ff481439c14bfc9ec054f638ca1e77
Isabella Urbano Trujillo <isabellaurbanot@gmail.com>
2023-09-12 14:52:57 +0200

    Changes were made

---
+++ Lab 2 - Version control.R
@@ -0,0 +1,25 @@
+
+# Lab - Session 2 - Version control ----------------
+# https://raw.githack.com/intro-to-data-science-22/labs/main/session-2-version-control/2-git.html
+
+
+# Set-up your script ------------------------------------------------------
+install.packages(c("tidyverse", "gapminder", "pacman")) # uncomment if already installed
+pacman::p_load(tidyverse, gapminder)
+
+# Load your Data into R ---------------------------------------------------
+data(gapminder)
+head(gapminder)
+
+# Clean your Data ---------------------------------------------------------
+gapminder_clean <- gapminder %>% 
+  rename(life_exp = lifeExp, gdp_per_cap = gdpPercap) %>% 
+  mutate(gdp = pop * gdp_per_cap)
+
+
+
+
+
+
+
+
